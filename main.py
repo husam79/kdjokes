bd677c466347aa84c20a7926590997c77543eb12
Husam Burhan <husam.burhan@gmail.com>
2024-10-01 22:36:11 +0300

    more features

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, JSONResponse
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+
 
 @app.get('/')
-def get_joke():
-    return "This is a joke!"
+def greet():
+  return JSONResponse(status_code=200, content={"joke": "my nice joke!"})
+
+
+@app.get('/text/')
+def greet_text():
+  return "hello!"
+
+
+@app.get('/pretty/', response_class=HTMLResponse)
+def greet_pretty(request: Request):
+  return templates.TemplateResponse('joke_template.html', {'request': request, 'joke': 'Hello!!!!'})
+
+
