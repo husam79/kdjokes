929f88b6aac139edfdd6cc3195f204c6201d7f81
Husam Burhan <husam.burhan@gmail.com>
2024-10-06 22:01:21 +0300

    Implement the required test to check the random selection of a joke

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,18 @@
 import unittest
+from unittest.mock import patch, mock_open
 from main import get_joke
 
-class TestJoke(unittest.TestCase):
+class JokeTesting(unittest.TestCase):
     def test_joke_length(self):
         joke = get_joke()
         self.assertGreater(len(joke), 3)
+    
+    @patch('builtins.open', new_callable=mock_open, read_data='[{"joke": "Joke 1"}, {"joke": "Joke 2"}, {"joke": "Joke 3"}]')
+    def test_joke_random_choosing(self, mock_file):
+        joke = get_joke()
+        mock_file.assert_called_once_with("jokes.json", 'r')
+        self.assertIn(joke, ["Joke 11", "Joke 12", "Joke 13"])
+        
 
 if __name__ == 'main':
     unittest.main()
