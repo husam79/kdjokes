998ab2dcfdf1b8b8c90bda988b7df1ed9bb58b17
Husam Burhan <husam.burhan@gmail.com>
2024-10-06 22:00:57 +0300

    get a random joke from the jokes.json file

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, JSONResponse
+import json
+from random import choice
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
+def get_random_joke():
+    jokes = []
+    with open('jokes.json', 'r') as file:
+        text = file.read()
+        jokes = json.loads(text)
+    
+    return choice(jokes)['joke']
+
 def get_joke():
-    return "This is my new joke!!"
+    return get_random_joke()
 
 @app.get('/')
 def greet():
