00a00b5c26b1f6a85a28e7f391b4c6edbe49df6f
Husam Burhan <husam.burhan@gmail.com>
2024-11-09 14:28:35 +0300

    using pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,8 @@
-import unittest
-from unittest.mock import patch, mock_open
+import pytest
 from main import get_joke
 
-class JokeTesting(unittest.TestCase):
-    def test_joke_length(self):
-        joke = get_joke()
-        self.assertGreater(len(joke), 1)
-    
-    @patch('builtins.open', new_callable=mock_open, read_data='[{"joke": "Joke 1"}, {"joke": "Joke 2"}, {"joke": "Joke 3"}]')
-    def test_joke_random_choosing(self, mock_file):
-        joke = get_joke()
-        mock_file.assert_called_once_with("jokes.json", 'r')
-        self.assertIn(joke, ["Joke 1", "Joke 2", "Joke 3"])
-        
+def test_joke_length():
+    joke = get_joke()
+    assert len(joke) > 1
 
-if __name__ == 'main':
-    unittest.main()
+    
